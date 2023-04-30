7f5ceff2e3f414094461d490a794b6fe14759ed5
Scott Steffes <swsteffes@gmail.com>
2023-04-30 14:15:01 -0500

    fix night start UI

---
+++ Assets/Scripts/UI/NightStartUI.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class NightStartUI : MonoBehaviour {
+    public RectTransform bgTransform;
+    public ShopUI shopUI;
+
+    public static readonly Vector2 ON_SCREEN_POS = new Vector2(0, 0);
+    public static readonly Vector2 OFF_SCREEN_POS = new Vector2(0, 1080);
+
+    public void Show(Action onCoverScreen, Action onComplete) {
+        Vector2 startPos = bgTransform.anchoredPosition;
+        this.CreateAnimationRoutine(1f, (float progress) => {
+            bgTransform.anchoredPosition = Vector2.Lerp(startPos, ON_SCREEN_POS, Easing.easeInSine(0, 1, progress));
+        }, () => {
+            onCoverScreen?.Invoke();
+            shopUI.Show(onComplete);
+            Continue();
+        });
+    }
+
+    private void Continue() {
+        StartCoroutine(ContinueRoutine());
+        IEnumerator ContinueRoutine() {
+            yield return new WaitForSecondsRealtime(1f);
+            Vector2 startPos = bgTransform.anchoredPosition;
+            this.CreateAnimationRoutine(1f, (float progress) => {
+                bgTransform.anchoredPosition = Vector2.Lerp(startPos, OFF_SCREEN_POS, Easing.easeInSine(0, 1, progress));
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ShopUI.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour {
+    public RectTransform bgTransform;
+    public Button continueButton;
+
+    private void Awake() {
+        continueButton.onClick.AddListener(Continue);
+    }
+
+    private Action onComplete;
+    public void Show(Action _onComplete) {
+        onComplete = _onComplete;
+        bgTransform.anchoredPosition = new Vector2(0, 0);
+    }
+
+    private void Continue() {
+        Vector2 startPos = bgTransform.anchoredPosition;
+        this.CreateAnimationRoutine(1f, (float progress) => {
+            bgTransform.anchoredPosition = Vector2.Lerp(startPos, NightStartUI.OFF_SCREEN_POS, Easing.easeInSine(0, 1, progress));
+        }, onComplete);
+    }
+}
