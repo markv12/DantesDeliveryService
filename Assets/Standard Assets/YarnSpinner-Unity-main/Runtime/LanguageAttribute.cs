f93c4631cb1738202e12da03513c5e38e29c2789
Scott Steffes <swsteffes@gmail.com>
2023-05-06 15:04:05 -0500

    remove yarn spinner

--- Assets/Standard Assets/YarnSpinner-Unity-main/Runtime/LanguageAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Yarn.Unity
-{
-    public class LanguageAttribute : PropertyAttribute
-    {
-        // No data or methods on this attribute; it's purely a marker.
-    }
-}
